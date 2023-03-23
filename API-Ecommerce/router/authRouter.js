b05703bb55f636e90323677153140e010c29b9fc
Nhuttruong61 <Nhuttruong2261@gmail.com>
2023-03-23 21:30:12 +0700

    kiểm tra user và admin

--- API-Ecommerce/router/authRouter.js
+++ API-Ecommerce/router/authRouter.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const { createUser, loginUserCtrl, getAllUser, getaUser, deleteaUser, updateUser } = require('../controller/userCtrl');
+const express = require("express");
+const { createUser, loginUserCtrl, getAllUser, getaUser, deleteaUser, updateUser } = require("../controller/userCtrl");
+const {authMiddleware, isAdmin} = require("../middlewares/authMiddleware")
 const router = express.Router();
+
 router.post('/register',createUser);
 router.post('/login', loginUserCtrl);
 router.get('/all-users',getAllUser)
-router.get('/:id', getaUser)
+router.get('/:id',authMiddleware,isAdmin, getaUser)
 router.delete('/:id', deleteaUser)
-router.put('/:id',updateUser)
+router.put('/edit-user',updateUser)
 
 module.exports = router;
